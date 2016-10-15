02820c1374d23af2608a5468fc7096f0c426a6cc
Rajan <walrajan@gmail.com>
2016-10-15 19:23:07 -0400

    adding type

--- src/lightning-auto.rkt
+++ src/lightning-auto.rkt
@@ -3,29 +3,439 @@
 (require ffi/unsafe)
 (define lightning-lib (ffi-lib "liblightning"))
 
-(define _jit_free_func_ptr _pointer)
-(define _jit_alloc_func_ptr* _pointer)
-(define _jit_state_t* _pointer)
-(define _jit_bool_t _pointer)
-(define _jit_gpr_t _pointer)
-(define _jit_word_t _pointer)
-(define _jit_word_t* _pointer)
-(define _jit_float32_t _pointer)
-(define _jit_realloc_func_ptr _pointer)
-(define _jit_pointer_t _pointer)
-(define _jit_code_t _pointer)
-(define _int _pointer)
-(define _jit_realloc_func_ptr* _pointer)
-(define _int* _pointer)
-(define _const_char* _pointer)
-(define _void _pointer)
+(define _void* _pointer);(_cpointer 'void* _pointer)
+
+(define _jit_free_func_ptr (_fun  _void* -> _void))
 (define _jit_free_func_ptr* _pointer)
+
+(define _jit_realloc_func_ptr (_fun _void* _size -> _void*))
+(define _jit_realloc_func_ptr* _pointer)
+
+(define _jit_alloc_func_ptr (_fun _size -> _void*))
+(define _jit_alloc_func_ptr* _pointer)
+
+(define _jit_state_t* _pointer)
+
+(define _jit_bool_t _int32)
+(define _jit_int32_t _int32)
+(define _jit_gpr_t _int32)
+(define _jit_fpr_t _int32)
+
+(define _jit_word_t (if (equal? (system-type 'word) 32) _int32 _int64))
+
+(define _jit_float32_t _float)
+(define _jit_float64_t _double)
+
+(define _int _int32)
+(define _int* _pointer)
+
+(define _jit_pointer_t _pointer)
+(define _const_char* _pointer)
 (define _jit_node_t* _pointer)
-(define _jit_fpr_t _pointer)
-(define _jit_int32_t _pointer)
+(define _jit_word_t* _pointer)
 (define _char** _pointer)
-(define _jit_alloc_func_ptr _pointer)
-(define _jit_float64_t _pointer)
+
+(define _jit_code_t
+  (_enum '(
+           jit_code_data
+           jit_code_live
+           jit_code_align
+           jit_code_save
+           jit_code_load
+           jit_code_name
+           jit_code_note
+           jit_code_label
+           jit_code_prolog
+           jit_code_ellipsis
+           jit_code_allocai
+           jit_code_allocar
+           jit_code_arg
+           jit_code_getarg_c
+           jit_code_getarg_uc
+           jit_code_getarg_s
+           jit_code_getarg_us
+           jit_code_getarg_i
+           jit_code_getarg_ui
+           jit_code_getarg_l
+           jit_code_putargr
+           jit_code_putargi
+           jit_code_va_start
+           jit_code_va_arg
+           jit_code_va_arg_d
+           jit_code_va_end
+           jit_code_addr
+           jit_code_addi
+           jit_code_addcr
+           jit_code_addci
+           jit_code_addxr
+           jit_code_addxi
+           jit_code_subr
+           jit_code_subi
+           jit_code_subcr
+           jit_code_subci
+           jit_code_subxr
+           jit_code_subxi
+           jit_code_rsbi
+           jit_code_mulr
+           jit_code_muli
+           jit_code_qmulr
+           jit_code_qmuli
+           jit_code_qmulr_u
+           jit_code_qmuli_u
+           jit_code_divr
+           jit_code_divi
+           jit_code_divr_u
+           jit_code_divi_u
+           jit_code_qdivr
+           jit_code_qdivi
+           jit_code_qdivr_u
+           jit_code_qdivi_u
+           jit_code_remr
+           jit_code_remi
+           jit_code_remr_u
+           jit_code_remi_u
+           jit_code_andr
+           jit_code_andi
+           jit_code_orr
+           jit_code_ori
+           jit_code_xorr
+           jit_code_xori
+           jit_code_lshr
+           jit_code_lshi
+           jit_code_rshr
+           jit_code_rshi
+           jit_code_rshr_u
+           jit_code_rshi_u
+           jit_code_negr
+           jit_code_comr
+           jit_code_ltr
+           jit_code_lti
+           jit_code_ltr_u
+           jit_code_lti_u
+           jit_code_ler
+           jit_code_lei
+           jit_code_ler_u
+           jit_code_lei_u
+           jit_code_eqr
+           jit_code_eqi
+           jit_code_ger
+           jit_code_gei
+           jit_code_ger_u
+           jit_code_gei_u
+           jit_code_gtr
+           jit_code_gti
+           jit_code_gtr_u
+           jit_code_gti_u
+           jit_code_ner
+           jit_code_nei
+           jit_code_movr
+           jit_code_movi
+           jit_code_extr_c
+           jit_code_extr_uc
+           jit_code_extr_s
+           jit_code_extr_us
+           jit_code_extr_i
+           jit_code_extr_ui
+           jit_code_htonr_us
+           jit_code_htonr_ui
+           jit_code_htonr_ul
+           jit_code_ldr_c
+           jit_code_ldi_c
+           jit_code_ldr_uc
+           jit_code_ldi_uc
+           jit_code_ldr_s
+           jit_code_ldi_s
+           jit_code_ldr_us
+           jit_code_ldi_us
+           jit_code_ldr_i
+           jit_code_ldi_i
+           jit_code_ldr_ui
+           jit_code_ldi_ui
+           jit_code_ldr_l
+           jit_code_ldi_l
+           jit_code_ldxr_c
+           jit_code_ldxi_c
+           jit_code_ldxr_uc
+           jit_code_ldxi_uc
+           jit_code_ldxr_s
+           jit_code_ldxi_s
+           jit_code_ldxr_us
+           jit_code_ldxi_us
+           jit_code_ldxr_i
+           jit_code_ldxi_i
+           jit_code_ldxr_ui
+           jit_code_ldxi_ui
+           jit_code_ldxr_l
+           jit_code_ldxi_l
+           jit_code_str_c
+           jit_code_sti_c
+           jit_code_str_s
+           jit_code_sti_s
+           jit_code_str_i
+           jit_code_sti_i
+           jit_code_str_l
+           jit_code_sti_l
+           jit_code_stxr_c
+           jit_code_stxi_c
+           jit_code_stxr_s
+           jit_code_stxi_s
+           jit_code_stxr_i
+           jit_code_stxi_i
+           jit_code_stxr_l
+           jit_code_stxi_l
+           jit_code_bltr
+           jit_code_blti
+           jit_code_bltr_u
+           jit_code_blti_u
+           jit_code_bler
+           jit_code_blei
+           jit_code_bler_u
+           jit_code_blei_u
+           jit_code_beqr
+           jit_code_beqi
+           jit_code_bger
+           jit_code_bgei
+           jit_code_bger_u
+           jit_code_bgei_u
+           jit_code_bgtr
+           jit_code_bgti
+           jit_code_bgtr_u
+           jit_code_bgti_u
+           jit_code_bner
+           jit_code_bnei
+           jit_code_bmsr
+           jit_code_bmsi
+           jit_code_bmcr
+           jit_code_bmci
+           jit_code_boaddr
+           jit_code_boaddi
+           jit_code_boaddr_u
+           jit_code_boaddi_u
+           jit_code_bxaddr
+           jit_code_bxaddi
+           jit_code_bxaddr_u
+           jit_code_bxaddi_u
+           jit_code_bosubr
+           jit_code_bosubi
+           jit_code_bosubr_u
+           jit_code_bosubi_u
+           jit_code_bxsubr
+           jit_code_bxsubi
+           jit_code_bxsubr_u
+           jit_code_bxsubi_u
+           jit_code_jmpr
+           jit_code_jmpi
+           jit_code_callr
+           jit_code_calli
+           jit_code_prepare
+           jit_code_pushargr
+           jit_code_pushargi
+           jit_code_finishr
+           jit_code_finishi
+           jit_code_ret
+           jit_code_retr
+           jit_code_reti
+           jit_code_retval_c
+           jit_code_retval_uc
+           jit_code_retval_s
+           jit_code_retval_us
+           jit_code_retval_i
+           jit_code_retval_ui
+           jit_code_retval_l
+           jit_code_epilog
+           jit_code_arg_f
+           jit_code_getarg_f
+           jit_code_putargr_f
+           jit_code_putargi_f
+           jit_code_addr_f
+           jit_code_addi_f
+           jit_code_subr_f
+           jit_code_subi_f
+           jit_code_rsbi_f
+           jit_code_mulr_f
+           jit_code_muli_f
+           jit_code_divr_f
+           jit_code_divi_f
+           jit_code_negr_f
+           jit_code_absr_f
+           jit_code_sqrtr_f
+           jit_code_ltr_f
+           jit_code_lti_f
+           jit_code_ler_f
+           jit_code_lei_f
+           jit_code_eqr_f
+           jit_code_eqi_f
+           jit_code_ger_f
+           jit_code_gei_f
+           jit_code_gtr_f
+           jit_code_gti_f
+           jit_code_ner_f
+           jit_code_nei_f
+           jit_code_unltr_f
+           jit_code_unlti_f
+           jit_code_unler_f
+           jit_code_unlei_f
+           jit_code_uneqr_f
+           jit_code_uneqi_f
+           jit_code_unger_f
+           jit_code_ungei_f
+           jit_code_ungtr_f
+           jit_code_ungti_f
+           jit_code_ltgtr_f
+           jit_code_ltgti_f
+           jit_code_ordr_f
+           jit_code_ordi_f
+           jit_code_unordr_f
+           jit_code_unordi_f
+           jit_code_truncr_f_i
+           jit_code_truncr_f_l
+           jit_code_extr_f
+           jit_code_extr_d_f
+           jit_code_movr_f
+           jit_code_movi_f
+           jit_code_ldr_f
+           jit_code_ldi_f
+           jit_code_ldxr_f
+           jit_code_ldxi_f
+           jit_code_str_f
+           jit_code_sti_f
+           jit_code_stxr_f
+           jit_code_stxi_f
+           jit_code_bltr_f
+           jit_code_blti_f
+           jit_code_bler_f
+           jit_code_blei_f
+           jit_code_beqr_f
+           jit_code_beqi_f
+           jit_code_bger_f
+           jit_code_bgei_f
+           jit_code_bgtr_f
+           jit_code_bgti_f
+           jit_code_bner_f
+           jit_code_bnei_f
+           jit_code_bunltr_f
+           jit_code_bunlti_f
+           jit_code_bunler_f
+           jit_code_bunlei_f
+           jit_code_buneqr_f
+           jit_code_buneqi_f
+           jit_code_bunger_f
+           jit_code_bungei_f
+           jit_code_bungtr_f
+           jit_code_bungti_f
+           jit_code_bltgtr_f
+           jit_code_bltgti_f
+           jit_code_bordr_f
+           jit_code_bordi_f
+           jit_code_bunordr_f
+           jit_code_bunordi_f
+           jit_code_pushargr_f
+           jit_code_pushargi_f
+           jit_code_retr_f
+           jit_code_reti_f
+           jit_code_retval_f
+           jit_code_arg_d
+           jit_code_getarg_d
+           jit_code_putargr_d
+           jit_code_putargi_d
+           jit_code_addr_d
+           jit_code_addi_d
+           jit_code_subr_d
+           jit_code_subi_d
+           jit_code_rsbi_d
+           jit_code_mulr_d
+           jit_code_muli_d
+           jit_code_divr_d
+           jit_code_divi_d
+           jit_code_negr_d
+           jit_code_absr_d
+           jit_code_sqrtr_d
+           jit_code_ltr_d
+           jit_code_lti_d
+           jit_code_ler_d
+           jit_code_lei_d
+           jit_code_eqr_d
+           jit_code_eqi_d
+           jit_code_ger_d
+           jit_code_gei_d
+           jit_code_gtr_d
+           jit_code_gti_d
+           jit_code_ner_d
+           jit_code_nei_d
+           jit_code_unltr_d
+           jit_code_unlti_d
+           jit_code_unler_d
+           jit_code_unlei_d
+           jit_code_uneqr_d
+           jit_code_uneqi_d
+           jit_code_unger_d
+           jit_code_ungei_d
+           jit_code_ungtr_d
+           jit_code_ungti_d
+           jit_code_ltgtr_d
+           jit_code_ltgti_d
+           jit_code_ordr_d
+           jit_code_ordi_d
+           jit_code_unordr_d
+           jit_code_unordi_d
+           jit_code_truncr_d_i
+           jit_code_truncr_d_l
+           jit_code_extr_d
+           jit_code_extr_f_d
+           jit_code_movr_d
+           jit_code_movi_d
+           jit_code_ldr_d
+           jit_code_ldi_d
+           jit_code_ldxr_d
+           jit_code_ldxi_d
+           jit_code_str_d
+           jit_code_sti_d
+           jit_code_stxr_d
+           jit_code_stxi_d
+           jit_code_bltr_d
+           jit_code_blti_d
+           jit_code_bler_d
+           jit_code_blei_d
+           jit_code_beqr_d
+           jit_code_beqi_d
+           jit_code_bger_d
+           jit_code_bgei_d
+           jit_code_bgtr_d
+           jit_code_bgti_d
+           jit_code_bner_d
+           jit_code_bnei_d
+           jit_code_bunltr_d
+           jit_code_bunlti_d
+           jit_code_bunler_d
+           jit_code_bunlei_d
+           jit_code_buneqr_d
+           jit_code_buneqi_d
+           jit_code_bunger_d
+           jit_code_bungei_d
+           jit_code_bungtr_d
+           jit_code_bungti_d
+           jit_code_bltgtr_d
+           jit_code_bltgti_d
+           jit_code_bordr_d
+           jit_code_bordi_d
+           jit_code_bunordr_d
+           jit_code_bunordi_d
+           jit_code_pushargr_d
+           jit_code_pushargi_d
+           jit_code_retr_d
+           jit_code_reti_d
+           jit_code_retval_d
+           jit_code_movr_w_f
+           jit_code_movr_ww_d
+           jit_code_movr_w_d
+           jit_code_movr_f_w
+           jit_code_movi_f_w
+           jit_code_movr_d_ww
+           jit_code_movi_d_ww
+           jit_code_movr_d_w
+           jit_code_movi_d_w
+           jit_code_last_code)))
+
 
 (define (get-error-thunk func-name)
   (lambda ()
@@ -40,13 +450,13 @@
   (get-ffi-obj
    "finish_jit"
    lightning-lib
-   (_fun _void -> _void)
+   (_fun -> _void)
    (get-error-thunk "finish_jit")))
 (define jit-new-state
   (get-ffi-obj
    "jit_new_state"
    lightning-lib
-   (_fun _void -> _jit_state_t*)
+   (_fun -> _jit_state_t*)
    (get-error-thunk "jit_new_state")))
 (define jit-clear-state
   (get-ffi-obj
